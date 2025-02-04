4ba023f39822fd14e8f3eedf561da2c1ce918742
Raeyan0786 <reyankhan01999@gmail.com>
2025-02-04 03:15:19 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="w-full bg-[#12372A]">
       <div className="flex flex-col justify-center items-center py-10">
-        <h1 className="font-extrabold text-4xl lg:text-6xl leading-4 text-white py-8 lg:py-14">Create Payment</h1>
+        <h1 className="font-extrabold text-4xl lg:text-6xl leading-4 text-white pt-8 lg:pb-2 pb-6 ">Create Payment</h1>
         <h1 className="font-extrabold text-4xl lg:text-6xl leading-4 text-white pb-6 lg:pb-8">Checkout Form</h1>
 
         <PaymentComponent/>
